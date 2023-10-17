8646f895194bb1950a924f2644f3bb87bffb73d1
fan xia <gonorth@qq.com>
2023-10-17 20:08:28 +0800

    简化依赖

--- prepub.cjs
+++
@@ -1,7 +0,0 @@
-const { writeFileSync } = require("fs")
-const { optionalDependencies } = require("./addon/package.json")
-const pkg = require("./package.json")
-if (optionalDependencies) {
-    pkg.optionalDependencies = optionalDependencies
-    writeFileSync("./package.json", JSON.stringify(pkg))
-}
