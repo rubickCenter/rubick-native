7d76c611042ee36e33e69089916e4035733a4d6c
fan xia <gonorth@qq.com>
2023-12-07 00:14:44 +0800

    fix

--- addon/postbuild.js
+++ addon/postbuild.js
@@ -3,7 +3,8 @@ const path = require("path");
 
 const ijs_path = path.join(__dirname, "index.js")
 const ijs = readFileSync(ijs_path, "utf-8")
-const twoijs = ijs.split('\n\nmodule.exports')
-const newijs = 'const nativeBinding = (new Function(`require`,`__dirname`,`' + twoijs[0].replaceAll('`', '\\`').replaceAll('$', '\\$') + '\nreturn nativeBinding`))(require,__dirname)\nmodule.exports' + twoijs[1]
+const twoijs = ijs.split('binding\`)\n}\n')
+
+const newijs = 'const nativeBinding = (new Function(`require`,`__dirname`,`' + twoijs[0].replaceAll('`', '\\`').replaceAll('$', '\\$') + 'binding\\`)\n}\nreturn nativeBinding`))(require,__dirname)\n' + twoijs[1]
 
 writeFileSync(ijs_path, newijs)
